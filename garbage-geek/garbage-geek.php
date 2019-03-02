1ea278d031d01a279b9b55a775bfa4bcb3318f70
Thomas Morgan Jr <tkmorgan@gmail.com>
2019-03-02 13:07:55 -0500

    Removes old plugin

--- garbage-geek/garbage-geek.php
+++ garbage-geek/garbage-geek.php
@@ -25,58 +25,16 @@
  * Domain Path:       /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+function create_post_type() {
+	register_post_type( 'geek_tip',
+		array(
+		'labels' => array(
+			'name' => __( 'Tips' ),
+			'singular_name' => __( 'Tip' )
+		),
+		'public' => true,
+		'has_archive' => true,
+		)
+	);
 }
-
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-garbage-geek-activator.php
- */
-function activate_garbage_geek() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-garbage-geek-activator.php';
-	Garbage_Geek_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-garbage-geek-deactivator.php
- */
-function deactivate_garbage_geek() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-garbage-geek-deactivator.php';
-	Garbage_Geek_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_garbage_geek' );
-register_deactivation_hook( __FILE__, 'deactivate_garbage_geek' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-garbage-geek.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_garbage_geek() {
-
-	$plugin = new Garbage_Geek();
-	$plugin->run();
-
-}
-run_garbage_geek();
+add_action( 'init', 'create_post_type' );
