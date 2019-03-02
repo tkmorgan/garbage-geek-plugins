658164bda85389e7aedb60bc35be02bfc66b8847
Emmanuel Porfirio <emmanuel@omnigecko.com>
2019-03-02 13:32:34 -0500

    updated Geek Tip post type

--- garbage-geek.php
+++ garbage-geek.php
@@ -25,22 +25,47 @@
  * Domain Path:       /languages
  */
 
-function create_geek_post_type() {
+function garbage_geek_plugin_create_geek_post_type() {
+	$lbls = array(
+		'name' => __('Trash Tips'),
+		'singular_name' => __('Trash Tip'),
+		'add_new'            => __( 'Add New Trash Tip' ),
+		'add_new_item'       => __( 'Add New Trash Tip' ),
+		'edit_item'          => __( 'Edit Trash Tip' ),
+		'new_item'           => __( 'Add New Trash Tip' ),
+		'view_item'          => __( 'View Trash Tip' ),
+		'search_items'       => __( 'Search Trash Tip' ),
+		'not_found'          => __( 'No Trash Tips found' ),
+		'not_found_in_trash' => __( 'No Trash Tip found in trash' )
+	);
+	$supports = array('title', 'thumbnail','editor');
 	register_post_type( 'geek_tip',
 		array(
-		'labels' => array(
-			'name' => __( 'Tips' ),
-			'singular_name' => __( 'Tip' )
-		),
+		'labels' => $lbls,
 		'public' => true,
 		'has_archive' => true,
+		'description'=> __('Post type for Trash Tips.'),
+		'capability_type' => 'post',
+		'rewrite'     => array( 'slug' => 'geek_tip'), // my custom slug
+		'has_archive' => true,
+		'supports' => $supports,
+		// 'register_meta_box_cb' => 'metaboxes_function'
 		)
 	);
 }
+add_action( 'init', 'garbage_geek_plugin_create_geek_post_type' );
 
-function remove_geek_post_type() {
-	unregister_post_type('geek_tip');
-}
-add_action( 'init', 'create_geek_post_type' );
-
-register_deactivation_hook( __FILE__, 'remove_geek_post_type' );
+//Activations
+    function garbage_geek_plugin_application_activation(){
+        garbage_geek_plugin_create_geek_post_type();
+        flush_rewrite_rules();
+    }
+    register_activation_hook( __FILE__, 'garbage_geek_plugin_application_activation' );
+//Deactivations
+    function garbage_geek_plugin_de_activation() {
+        unregister_post_type( 'geek_tip');
+        flush_rewrite_rules();
+    }
+    // register_deactivation_hook( __FILE__, 'whisper_room_plugin_application_de_activation' );
+//Unsinstallation
+?>
