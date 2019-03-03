d9dced2a48e2395f564911ded74cefcb215920ad
Thomas Morgan Jr <tkmorgan@gmail.com>
2019-03-02 23:21:31 -0500

    refactors and adds center type field to rc_totals

--- garbage-geek.php
+++ garbage-geek.php
@@ -67,45 +67,17 @@ include('helpers/recyclingCenterTotals.php');
 	//Geek_tip - encoragement or critisizm
 
 
-	function save_generic_category_fields_meta( $field_name, $post_id, $post) {   
-		
-		// Return if the user doesn't have edit permissions.
-		if ( ! current_user_can( 'edit_post', $post_id ) ) {
-			return $post_id;
-		}
-		// Verify this came from the our screen and with proper authorization,
-		// because save_post can be triggered at other times.
-		if ( ! isset( $_POST[$field_name] ) || ! wp_verify_nonce( $_POST[$field_name . '_fields'], basename(__FILE__) ) ) {
-			return $post_id;
-		}
-		// Now that we're authenticated, time to save the data.
-		// This sanitizes the data from the field and saves it into an array $events_meta.
-		$events_meta[$field_name] = esc_textarea( $_POST[$field_name] );
-		// Cycle through the $events_meta array.
-		// Note, in this example we just have one item, but this is helpful if you have multiple.
-		foreach ( $events_meta as $key => $value ) :
-			// Don't store custom data twice
-			if ( 'revision' === $post->post_type ) {
-				return;
-			}
-			if ( get_post_meta( $post_id, $key, false ) ) {
-				// If the custom field already has a value, update it.
-				update_post_meta( $post_id, $key, $value );
-			} else {
-				// If the custom field doesn't have a value, add it.
-				add_post_meta( $post_id, $key, $value);
-			}
-			if ( ! $value ) {
-				// Delete the meta key if there's no value
-				delete_post_meta( $post_id, $key );
-			}
-		endforeach;
-	}
-
 	// rc_totals 
 	function save_rc_totals_rc_totals_fields_meta( $post_id, $post) { 
-		foreach(array_keys( recyclingCenterTotals::$types ) as $key)
-			save_generic_category_fields_meta( $key, $post_id, $post );
+		foreach(array_keys( recyclingCenterTotals::$recyclable_types ) as $key)
+		recyclingCenterTotals::save_generic_category_fields_meta( $key, $post_id, $post );
+		
+			// save our center type field
+			recyclingCenterTotals::save_generic_category_fields_meta( 
+			recyclingCenterTotals::$center_type['slug'], 
+			$post_id, 
+			$post 
+		);
 	}
 
 	add_action( 'save_post', 'save_rc_totals_rc_totals_fields_meta',1 ,2 );
@@ -155,24 +127,10 @@ function garbage_geek_plugin_rc_totals_post_metaboxes(){
 	//Tip Catagory
 	function garbage_geek_plugin_rc_totals(){
 		global $post;
-		foreach( recyclingCenterTotals::$types as $slug => $name ) {
-			wp_nonce_field( basename( __FILE__ ), "${slug}_fields" );
-			$meta_val = get_post_meta( $post->ID, $slug, true );
-			?>
-			<div>
-				<label for="<?=$slug?>">
-					<?=$name;?>
-					<input 
-						type='text' 
-						value='<?=$meta_val?>' 
-						id='<?=$slug?>' 
-						name="<?=$slug?>" 
-						/>
-				</label>
-			</div>
-			<?php
-		}
+		recyclingCenterTotals::get_metaboxes_for_text_fields($post);
+		recyclingCenterTotals::get_metabox_for_rc_center_category_field($post);
 	}
+
 	add_meta_box(
 		'rc_totals',
 		'Recycling Center Counts',
@@ -201,7 +159,7 @@ function garbage_geek_plugin_rc_totals_post_metaboxes(){
 			'not_found'          => __( 'No Recycling Center Totals found' ),
 			'not_found_in_trash' => __( 'No Recycling Center Total found in trash' )
 		);
-		$supports = array('title', 'thumbnail','editor','catagories');
+		$supports = array('title'/*, 'thumbnail','editor'*/,'catagories');
 		register_post_type( 'rc_totals',
 			array(
 			'labels' => $lbls,
@@ -335,3 +293,58 @@ function garbage_geek_admin_add_email() {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Mail Handeling:
+    //mailgun request
+    function sendMailgun ($subject, $msg){
+        //Send Email
+        $response = wp_remote_post('https://us-central1-garbage-geek.cloudfunctions.net/mailgun-send', array(
+            'headers' => array(
+                'from' => "Garbage Geeks",
+                'subject' => $subject
+            ),  
+            'body'=> array(
+                'name' => $msg
+            )
+        ));
+    }   
+    //Main Tasks
+    function whispermail_send_email_to_admin() {
+        if (isset($_POST['form-submited'])){
+            $frm = $_POST['form-submited'];
+        }else{
+            return;
+        }
+        //form Selection
+        if($frm == "hm-pg-contact"){
+            $reqName = sanitize_user($_POST['name']);
+            $subject = "Home Page - Contact - Form Fill For: ".$reqName;
+            $reqEmail = sanitize_email($_POST['email']);
+            $reqMsg = sanitize_textarea_field($_POST['msg']); 
+            sendMailgun($subject, "Sender: $reqName Email: $reqEmail Msg: $reqMsg");
+            wp_redirect( get_home_url()."/contact-response/");
+        }else{
+            wp_redirect( get_home_url()."/404/");
+        }
+    }
+    add_action( 'admin_post_nopriv_contact_form', 'whispermail_send_email_to_admin' );
+    add_action( 'admin_post_contact_form', 'whispermail_send_email_to_admin' );
+?>
--- helpers/recyclingCenterTotals.php
+++ helpers/recyclingCenterTotals.php
@@ -1,6 +1,6 @@
 <?php
 class recyclingCenterTotals {
-    public static $types = [
+    public static $recyclable_types = [
         'aluminum' => 'Aluminum',
         'plastics-commingled' => 'Plastics ("Commingled")',
         'glass' => 'Glass',
@@ -10,4 +10,113 @@ class recyclingCenterTotals {
         'newspaper' => 'Newspaper',
         'all-mixed-no-glass' => 'All Mixed (no glass)'
     ];
+
+    public static $center_type = [
+        'slug' => 'center-type',
+        'name' => 'Center Type',
+        'options' => [
+            'drop-off-centers' => 'Drop Off Centers',
+            'curbside' => 'Curbside',
+            'other' => 'Other'    
+        ]
+    ];
+
+    public static function get_metabox_text_markup( $slug, $name, $meta_val ) {
+        ?>
+        <div>
+            <label for="<?=$slug?>">
+                <?=$name;?>
+                <input 
+                    type='text' 
+                    value='<?=$meta_val?>' 
+                    id='<?=$slug?>' 
+                    name="<?=$slug?>" 
+                    />
+            </label>
+        </div>
+        <?php
+    }
+
+    //function to check if selected
+    public static function selectedChk ($compare, $pluginTip){
+        $compare = sanitize_text_field($compare);
+        if($pluginTip == $compare){
+            return 'value="'.$compare.'" Selected';
+        }else{
+            return 'value="'.$compare.'"';
+        }
+    }
+
+
+    public static function get_metabox_option_markup( $slug, $name, $meta_val ) {
+        ?>
+        <div>
+            <label for="<?=$slug?>">
+                <?=$name;?>
+                <select id='<?=$slug?>' 
+                    name="<?=$slug?>" 
+                >
+                    <option value=''>Select</option>
+                    <?foreach( self::$center_type['options'] as $tslug => $tname ):?>
+                        <option <?=self::selectedChk($tslug, $meta_val)?>><?=$tname?></option>
+                    <?endforeach;?>
+                </select>
+            </label>
+        </div>
+        <?php
+    }
+
+    public static function save_generic_category_fields_meta( $field_name, $post_id, $post) {   
+		
+		// Return if the user doesn't have edit permissions.
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return $post_id;
+		}
+
+		// Verify this came from the our screen and with proper authorization,
+		// because save_post can be triggered at other times.
+		if ( ! isset( $_POST[$field_name] ) || ! wp_verify_nonce( $_POST[$field_name . '_fields'], basename(__FILE__) ) ) {
+			return $post_id;
+		}
+		// Now that we're authenticated, time to save the data.
+		// This sanitizes the data from the field and saves it into an array $events_meta.
+		$events_meta[$field_name] = esc_textarea( $_POST[$field_name] );
+		// Cycle through the $events_meta array.
+		// Note, in this example we just have one item, but this is helpful if you have multiple.
+		foreach ( $events_meta as $key => $value ) :
+			// Don't store custom data twice
+			if ( 'revision' === $post->post_type ) {
+				return;
+			}
+			if ( get_post_meta( $post_id, $key, false ) ) {
+				// If the custom field already has a value, update it.
+				update_post_meta( $post_id, $key, $value );
+			} else {
+				// If the custom field doesn't have a value, add it.
+				add_post_meta( $post_id, $key, $value);
+			}
+			if ( ! $value ) {
+				// Delete the meta key if there's no value
+				delete_post_meta( $post_id, $key );
+			}
+		endforeach;
+	}
+
+    public static function get_metaboxes_for_text_fields($post) {
+        foreach( self::$recyclable_types as $slug => $name ) {
+			wp_nonce_field( basename( __FILE__ ), "${slug}_fields" );
+            $meta_val = get_post_meta( $post->ID, $slug, true );
+            self::get_metabox_text_markup( $slug, $name, $meta_val );
+		}
+    }
+
+    public static function get_metabox_for_rc_center_category_field($post) {
+        $slug = self::$center_type['slug'];
+        $name = self::$center_type['name'];
+
+        wp_nonce_field( basename( __FILE__ ), "${slug}_fields" );
+        $meta_val = get_post_meta( $post->ID, $slug, true );
+
+        self::get_metabox_option_markup( $slug, $name, $meta_val );
+    }
 }
